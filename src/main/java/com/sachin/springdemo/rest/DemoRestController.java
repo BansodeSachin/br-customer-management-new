2e00e40df2027d955fb0de9a604a047a7eed562c
Datta Angad Shinge <dattashinge8@gmail.com>
2024-02-12 17:45:52 +0530

    Datta Shinge Commit message

--- src/main/java/com/sachin/springdemo/rest/DemoRestController.java
+++ src/main/java/com/sachin/springdemo/rest/DemoRestController.java
@@ -18,5 +18,17 @@ public class DemoRestController {
 		sb.append(", and I am thrilled to have the opportunity to introduce myself. I am a [Your Profession or Student Major], currently [your current position or academic status]. With a passion for [mention a specific interest or field related to your profession], I have dedicated myself to [any relevant experience or achievements]. My journey has equipped me with valuable skills in [mention key skills], and I am always eager to learn and grow. Outside of [work or studies], I enjoy [hobbies or interests], which contribute to my well-rounded personality. I am excited to connect, collaborate, and embark on new challenges, and I look forward to the experiences and opportunities that the future holds.");
 		
 		return sb.toString(); 
+		
 	}
+	
+	@GetMapping("/getDattaIntro")
+		
+		public String getDattaIntro(){
+			StringBuilder sb1 = new StringBuilder();
+			sb1.append("hello my name is Datta Shinge");
+			sb1.append("From Latur Maharastra");
+			sb1.append("<h1>My short-term goal is to get placed in a reputed company where I can improve my skills and knowledge.I developed many static and dynamic websites. I really enjoyed developing websites for to-do lists. It helps users keep track of their tasks.</h1>");
+			return sb1.toString();
+		}
+
 }
